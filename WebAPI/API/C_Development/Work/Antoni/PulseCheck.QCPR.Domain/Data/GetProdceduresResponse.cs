2daa971112571eee7892c137472332810d64e8ed
Winston Murdock (HARRIS\wm85227) <WMurdock@harriscomputer.com>
2020-05-04 14:58:16 +0000

    Branched from $/WebAPI/API/C_Development/C_DevTrunk
    git-tfs-id: [http://ros-tfs-px01.picis.com:8080/tfs/emar]$/;C24

---
+++ WebAPI/API/C_Development/Work/Antoni/PulseCheck.QCPR.Domain/Data/GetProdceduresResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace PulseCheck.QCPR.Domain.Data
+{
+    public class GetProdceduresResponse
+    {
+        public int SiteId { get; set; }
+
+        public string Code { get; set; }
+
+        public string Facility { get; set; }
+
+        public string Interface { get; set; }
+
+        public string Name { get; set; }
+    }
+}
