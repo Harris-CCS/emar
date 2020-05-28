3d09e2c7b2317db154be65dcb74dd2b9a3bd3361
Winston Murdock (HARRIS\wm85227) <WMurdock@harriscomputer.com>
2020-05-28 18:10:29 +0000

    Updated the jQuery packages.  This resolved the build error with them.
    git-tfs-id: [http://ros-tfs-px01.picis.com:8080/tfs/emar]$/;C39

---
+++ WebAPI/API/C_Development/Work/Winston/PulseCheck.API/Scripts/_references.js
@@ -0,0 +1,6 @@
+﻿/// <reference path="jquery-3.5.1.js" />
+/// <autosync enabled="true" />
+/// <reference path="ai.0.22.19-build00125.min.js" />
+/// <reference path="bootstrap.js" />
+/// <reference path="modernizr-2.8.3.js" />
+/// <reference path="respond.js" />
