813927e9f3c10e47c73adc0faa28f58c56d6f191
dm85223 <HARRIS\dm85223>
2020-06-25 00:42:44 +0000

    emar-102 additions for site configuration
    git-tfs-id: [http://ros-tfs-px01.picis.com:8080/tfs/emar]$/;C90

--- Database/C_Development/C_DevTrunk/Tables/options.sql
+++ Database/C_Development/C_DevTrunk/Tables/options.sql
@@ -1,11 +1,9 @@
-create table [dbo].[global_options]
-(
-    [id]          [int] identity(1, 1) not null
-  , [name]        [varchar](40) not null
-  , [description] [varchar](1000) not null
-  , [position]    [tinyint] not null
-  , constraint [pk__global_options__id] primary key clustered([id] asc)
-);
+create table [dbo].[options]
+    (
+      [id]          [int] identity(1, 1) not null
+    , [name]        [varchar](40) not null
+    , [description] [varchar](1000) not null
+    , constraint [pk__options__id] primary key clustered([id] asc));
 go
 
 /********
@@ -35,9 +33,9 @@ execute [sys].[sp_addextendedproperty]
   , @level0type = N'SCHEMA'
   , @level0name = N'dbo'
   , @level1type = N'TABLE'
-  , @level1name = N'global_options'
+  , @level1name = N'options'
   , @level2type = N'CONSTRAINT'
-  , @level2name = N'pk__global_options__id';
+  , @level2name = N'pk__options__id';
 go
 
 /***************
@@ -47,11 +45,11 @@ go
 
 execute [sys].[sp_addextendedproperty]
     @name = N'MS_Description'
-  , @value = N'This table contains a list of global operations options for the installation'
+  , @value = N'This table contains a list of options referenced by the site_options table'
   , @level0type = N'SCHEMA'
   , @level0name = N'dbo'
   , @level1type = N'TABLE'
-  , @level1name = N'global_options';
+  , @level1name = N'options';
 go
 
 /***************
@@ -65,7 +63,7 @@ execute [sys].[sp_addextendedproperty]
   , @level0type = N'SCHEMA'
   , @level0name = N'dbo'
   , @level1type = N'TABLE'
-  , @level1name = N'global_options'
+  , @level1name = N'options'
   , @level2type = N'COLUMN'
   , @level2name = N'id';
 go
@@ -76,7 +74,7 @@ execute [sys].[sp_addextendedproperty]
   , @level0type = N'SCHEMA'
   , @level0name = N'dbo'
   , @level1type = N'TABLE'
-  , @level1name = N'global_options'
+  , @level1name = N'options'
   , @level2type = N'COLUMN'
   , @level2name = N'name';
 go
@@ -87,18 +85,7 @@ execute [sys].[sp_addextendedproperty]
   , @level0type = N'SCHEMA'
   , @level0name = N'dbo'
   , @level1type = N'TABLE'
-  , @level1name = N'global_options'
+  , @level1name = N'options'
   , @level2type = N'COLUMN'
   , @level2name = N'description';
-go
-
-execute [sys].[sp_addextendedproperty]
-    @name = N'MS_Description'
-  , @value = N'Option position'
-  , @level0type = N'SCHEMA'
-  , @level0name = N'dbo'
-  , @level1type = N'TABLE'
-  , @level1name = N'global_options'
-  , @level2type = N'COLUMN'
-  , @level2name = N'position';
 go
