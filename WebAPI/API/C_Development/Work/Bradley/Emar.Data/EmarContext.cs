dd7c3b49448b8427dcad90816668f398efc8928e
bm70142 <HARRIS\bm70142>
2020-06-26 19:50:32 +0000

    Including the Emar Context (inadvertently left out)
    git-tfs-id: [http://ros-tfs-px01.picis.com:8080/tfs/emar]$/;C97

---
+++ WebAPI/API/C_Development/Work/Bradley/Emar.Data/EmarContext.cs
@@ -0,0 +1,27 @@
+﻿using Emar.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Emar.Data
+{
+    public class EmarContext : DbContext
+    {
+        private DbContextOptions _options;
+
+        public EmarContext(DbContextOptions options) : base(options)
+        {
+            _options = options;
+        }
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    if (!optionsBuilder.IsConfigured)
+        //    {
+        //        // Only used for development - will use more sophisticated methods when beyond dev...
+        //        optionsBuilder.UseSqlServer(_options.Extensions);
+        //            "Data Source = (localdb)\\SQL2016; Initial Catalog = EMAR");
+        //    }
+        //}
+    }
+}
