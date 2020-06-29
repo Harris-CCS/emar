7cce62e7a0ce70807df44ee5c5e51db6282e7063
sg91136 <HARRIS\sg91136>
2020-06-29 18:54:13 +0000

    Routing V0 + patient service V0 (merge from sylvie)
    git-tfs-id: [http://ros-tfs-px01.picis.com:8080/tfs/emar]$/;C103

---
+++ Presentation/C_Development/C_DevTrunk/emar/src/app/mockup/patients.ts
@@ -0,0 +1,28 @@
+import { Patient } from '../interfaces/patient';
+
+export const PATIENTS: Patient[] = [
+{
+        "id": 1,
+        "firstName": "Tony",
+        "lastName": "Stark",
+        "middleName": "B.",
+        "fullName": "Tony B. Stark",
+        "age": 56,
+        "ageUnit": "years",
+        "dateOfBirth": "",
+        "gender": "M",
+        "bedCode": "ER08"
+    },
+    {
+        "id":2,
+        "firstName": "Wenda",
+        "lastName": "Frank",
+        "middleName": "",
+        "fullName": "Wenda Frank",
+        "age": 2,
+        "ageUnit": "months",
+        "dateOfBirth": "",
+        "gender": "F",
+        "bedCode": "A01"
+    }
+]
