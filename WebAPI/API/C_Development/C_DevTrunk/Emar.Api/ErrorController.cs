b06935d15a2b71f5ea781ec33213629b5940c227
ab70004 <ab70004@harris.tfs.local>
2020-07-02 19:45:20 +0000

    git-tfs-id: [http://ros-tfs-px01.picis.com:8080/tfs/emar]$/;C121

---
+++ WebAPI/API/C_Development/C_DevTrunk/Emar.Api/ErrorController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Emar.Api
+{
+    [Route("api/[controller]")]
+    public class ErrorController : Controller
+    {
+        [Route("")]
+        [AllowAnonymous]
+        public IActionResult Get()
+        {
+            // Get the details of the exception that occurred
+            var exceptionFeature = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+
+            if (exceptionFeature != null)
+            {
+                // Get which route the exception occurred at
+                string routeWhereExceptionOccurred = exceptionFeature.Path;
+
+                // Get the exception that occurred
+                Exception exceptionThatOccurred = exceptionFeature.Error;
+
+                // TODO: Do something with the exception
+                // Log it with Serilog?
+                // Send an e-mail, text, fax, or carrier pidgeon?  Maybe all of the above?
+                // Whatever you do, be careful to catch any exceptions, otherwise you'll end up with a blank page and throwing a 500
+            }
+
+            /////return StatusCode(StatusCodes.Status500InternalServerError);
+            return BadRequest();
+        }
+    }
+}
--- WebAPI/API/C_Development/C_DevTrunk/Emar.Core/Patients/Repository/PatientRepository.cs
+++ WebAPI/API/C_Development/C_DevTrunk/Emar.Core/Patients/Repository/PatientRepository.cs
@@ -1,5 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using Emar.Core;
+using Emar.Core.Patients.Model;
+using Emar.Core.Patients.Service;
 using Emar.Data;
 using Emar.Data.Entities;
 
@@ -8,17 +12,30 @@ namespace Emar.Core.Patients.Repository
     public class PatientRepository : IPatientRepository
     {
         private readonly EmarContext _context;
+#if PAGING || SORTING || EXPANDO
+        private readonly IPropertyMappingService _propertyMappingService;
+#endif
 
         public PatientRepository()
         {
 
         }
 
+#if ORIGINAL
         public PatientRepository(EmarContext emarContext)
         {
-            _context = emarContext;
+            _context = emarContext ?? throw new ArgumentNullException(nameof(emarContext));
         }
+#endif
+#if PAGING || SORTING || EXPANDO
+        public PatientRepository(EmarContext emarContext, IPropertyMappingService propertyMappingService)
+        {
+            _context = emarContext ?? throw new ArgumentNullException(nameof(emarContext));
+            _propertyMappingService = propertyMappingService ?? throw new ArgumentNullException(nameof(propertyMappingService));
+        }
+#endif
 
+#if ORIGINAL
         public IEnumerable<Patient> GetPatients(ResourceParameters resourceParameters)
         {
             var patients = _context.Patients.AsEnumerable();
@@ -35,6 +52,33 @@ namespace Emar.Core.Patients.Repository
 
             return patients;
         }
+#endif
+#if PAGING || SORTING || EXPANDO
+        public PagedList<Patient> GetPatients(ResourceParameters resourceParameters)
+        {
+            var patients = _context.Patients.AsEnumerable();
+
+            if (!resourceParameters.IncludeInactive)
+            {
+                patients = patients.Where(pt => pt.Active.Equals(true));
+            }
+
+            if (resourceParameters.Site != null)
+            {
+                patients = patients.Where(pt => pt.SiteId.Equals(resourceParameters.Site));
+            }
+
+            if (resourceParameters.OrderBy != null)
+            {
+                //get property mapping dictionary
+                var propertyMappingDictionary = _propertyMappingService.GetPropertyMapping<PatientDto, Patient>();
+
+                patients = patients.AsQueryable().ApplySort(resourceParameters.OrderBy, propertyMappingDictionary);
+            }
+
+            return PagedList<Patient>.Create(patients.AsQueryable(), resourceParameters.PageNumber, resourceParameters.PageSize);
+        }
+#endif
 
         public Patient GetPatient(long? patientId, ResourceParameters resourceParameters)
         {
