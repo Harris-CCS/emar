44dcd72426630fd77b66e18932a79ec054c78508
sg91136 <HARRIS\sg91136>
2020-07-06 20:51:44 +0000

    page name in header (merged from Sylvie)
    git-tfs-id: [http://ros-tfs-px01.picis.com:8080/tfs/emar]$/;C131

--- Presentation/C_Development/C_DevTrunk/emar/src/app/app-routing.module.ts
+++ Presentation/C_Development/C_DevTrunk/emar/src/app/app-routing.module.ts
@@ -2,16 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { OrderEntryComponent } from '../pages/order-entry/order-entry.component';
-import { MedComposerComponent } from '../pages/med-composer/med-composer.component';
+import { ComposerMedComponent } from '../pages/composer-med/composer-med.component';
 import { PatientsDashboardComponent} from '../pages/patients-dashboard/patients-dashboard.component';
 import { ErrorPageComponent } from '../shared/component/error-page/error-page.component';
 
 const routes: Routes = [
-    { path: '', redirectTo: './patients', pathMatch: 'full' },
-    { path: 'patients', component: PatientsDashboardComponent},
-    { path: 'patients/:id/orders/:idOrder', component: MedComposerComponent },
-    { path: 'patients/:id/orders', component: OrderEntryComponent },
-    { path: 'patients/:id/new-order', component: MedComposerComponent },
+    { path: '', redirectTo: 'patients/1/orders', pathMatch: 'full' },
+    { path: 'patients', component: PatientsDashboardComponent, data: {title: 'Department'}},
+    { path: 'patients/:id/orders/:idOrder', component: ComposerMedComponent, data: {title: 'Composer'} },
+    { path: 'patients/:id/orders', component: OrderEntryComponent, data: {title: 'Medication Services'} },
+    { path: 'patients/:id/new-order', component: ComposerMedComponent, data: {title: 'Composer'} },
     { path: 'not-found', component: ErrorPageComponent, data: {message: 'Page not found!'} },
     { path: '**', redirectTo: '/not-found'}
 ];
