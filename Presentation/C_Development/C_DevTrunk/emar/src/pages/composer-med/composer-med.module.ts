298624b688667f3ace0f95f1685242ab5e5de3ce
sg91136 <HARRIS\sg91136>
2020-07-27 18:30:46 +0000

    EMAR-182 mockup order detail form (from sylvie)
    git-tfs-id: [http://ros-tfs-px01.picis.com:8080/tfs/emar]$/;C243

--- Presentation/C_Development/C_DevTrunk/emar/src/pages/composer-med/composer-med.module.ts
+++ Presentation/C_Development/C_DevTrunk/emar/src/pages/composer-med/composer-med.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { ComposerMedComponent } from './composer-med.component';
+import { DetailFormComponent } from './detail-form/detail-form.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  declarations: [ComposerMedComponent],
+  declarations: [
+    ComposerMedComponent,
+    DetailFormComponent
+  ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
+    NgbModule,
+    FormsModule
   ],
   exports: [
     ComposerMedComponent,
