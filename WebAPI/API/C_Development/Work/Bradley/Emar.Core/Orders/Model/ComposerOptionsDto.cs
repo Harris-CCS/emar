9daa4f784ec157c2c9a400e0d4b211a8d2f0ab2d
bm70142 <HARRIS\bm70142>
2020-07-28 22:06:38 +0000

    EMAR-173:  Some Mock Composer Setup components to produce a proposed JSON shape.
    git-tfs-id: [http://ros-tfs-px01.picis.com:8080/tfs/emar]$/;C261

---
+++ WebAPI/API/C_Development/Work/Bradley/Emar.Core/Orders/Model/ComposerOptionsDto.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using Emar.Core.Medications.Model;
+
+namespace Emar.Core.Orders.Model
+{
+    public class ComposerOptionsDto
+    {
+        public string BrandName { get; set; }
+        public IEnumerable<FormStrengthDto> AvailableFormStrength { get; set; }
+    }
+
+    public class FormStrengthDto
+    {
+        public int Id { get; set; }
+        public string FormStrengthName { get; set; }
+        public IEnumerable<MedicationRouteDto> AvailableRoutes { get; set; }
+        public IEnumerable<PreferredDoseDto> PreferredDoses { get; set; }
+        public IEnumerable<MedicationRouteDto> PreferredRoutes { get; set; }
+        public IEnumerable<FrequencyDto> PreferredFrequencies { get; set; }
+    }
+
+    public class PreferredDoseDto
+    {
+        public string DoseName { get; set; }
+        public decimal Dose { get; set; }
+        public UnitDto DoseUnit { get; set; }
+    }
+
+    public class UnitDto
+    {
+        public int Id { get; set; }
+        public string UnitName { get; set; }
+    }
+
+    public class FrequencyDto
+    {
+        public int Id { get; set; }
+        public string FrequencyName { get; set; }
+    }
+}
