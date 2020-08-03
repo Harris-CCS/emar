53cf2fe9c8cc917206d9cd172b0163fae3da5212
sg91136 <HARRIS\sg91136>
2020-08-03 11:32:02 +0000

    EMAR-185 frequency mockup + sass variables (from sylvie)
    git-tfs-id: [http://ros-tfs-px01.picis.com:8080/tfs/emar]$/;C296

--- Presentation/C_Development/C_DevTrunk/emar/src/pages/composer-med/frequency-form/frequency-form.component.ts
+++ Presentation/C_Development/C_DevTrunk/emar/src/pages/composer-med/frequency-form/frequency-form.component.ts
@@ -1,9 +1,12 @@
 import { Component, Output, EventEmitter, OnInit, Input } from '@angular/core';
-import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { debounceTime, distinctUntilChanged, map } from 'rxjs/operators';
 
 import { Frequency } from '../../../app/interfaces/frequency';
-import { FormStrength } from '../../../app/interfaces/formStrength';
 import { FREQUENCIES } from '../../../app/mockup/frequencies';
+const DURATION_UNITS: string[] = ["Doses", "Hours", "Days"]
+
 
 @Component({
     selector: 'frequency-form',
@@ -17,18 +20,39 @@ export class FrequencyFormComponent implements OnInit {
     @Input() preferredFrequencies: Frequency[] = [];
     selectedFrequency: Frequency = null; // TODO come from composer
     frequencies: Frequency[] = FREQUENCIES; // TODO API
+    selectedDuration: string = ''; // TODO come from composer
+    preferredDurationUnits: string[] = DURATION_UNITS;
 
     constructor(private fb: FormBuilder) {}
 
     ngOnInit() {
         this.frequencyForm = this.fb.group({
-            'frequency': new FormControl(null)
+            'frequency': new FormControl(null, Validators.required),
+            'duration': new FormControl(null),
+            'durationUnit': new FormControl(null),
+            'startTime': new FormControl(null),
+            'endTime': new FormControl(null)
         });
         this.formReady.emit(this.frequencyForm);
     }
 
-    changeSelectedFrequency(frequency: Frequency) {
-        this.selectedFrequency = frequency;
-        this.frequencyForm.controls['frequency'].setValue(frequency.frequencyName);  // TODO id?
+    lookupFrequency(text$: Observable<string>) {
+        return text$.pipe(
+            debounceTime(200),
+            distinctUntilChanged(),
+            map(term => term.length < 1 ? []
+            : FREQUENCIES.filter(f => new RegExp(term, 'mi').test(f.frequencyName)).slice(0, 10))
+            // TODO this.frequencies but this is undefined
+        )
+    }
+
+    formatFrequency = (frequency: Frequency) => frequency.frequencyName
+
+    usePreferredFrequency(frequency: Frequency) {
+        this.frequencyForm.patchValue({'frequency': frequency});
+    }
+
+    onDurationUnit(unit: string) {
+        this.frequencyForm.patchValue({'durationUnit': unit});
     }
 }
