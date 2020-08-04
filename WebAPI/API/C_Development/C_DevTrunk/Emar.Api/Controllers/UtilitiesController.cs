d7aafcc05d59dae76ff717c5529c9ec75421f602
bm70142 <HARRIS\bm70142>
2020-08-04 16:06:45 +0000

    EMAR-160: Adding HATEOAS links EMAR-201: Initial pass at a Utility to bring EF model in synch with a database (own time - not yet usable)
    git-tfs-id: [http://ros-tfs-px01.picis.com:8080/tfs/emar]$/;C307

---
+++ WebAPI/API/C_Development/C_DevTrunk/Emar.Api/Controllers/UtilitiesController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Emar.Data;
+using Emar.Data.Helpers;
+
+namespace Emar.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UtilitiesController : ControllerBase
+    {
+        private readonly EmarContext _context;
+
+        public UtilitiesController(EmarContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Utilities
+        [HttpGet("ConfirmEfConfiguration")]
+        public ActionResult<string> ConfirmEfConfiguration()
+        {
+            try
+            {
+                new EfToDbSynchHelper(_context).CompareEfToDb();
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return Problem(e.Message);
+            }
+        }
+    }
+}
