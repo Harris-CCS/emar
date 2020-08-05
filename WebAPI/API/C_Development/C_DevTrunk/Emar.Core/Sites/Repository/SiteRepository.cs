bce0863908fbd99cb4293fdf36e5ca0c0dc06d89
bm70142 <HARRIS\bm70142>
2020-08-05 07:47:38 +0000

    EMAR-58: Adding API call to retrieve Department Preferred List (by Site_id and optionally by Department Code).  Also, if the PatientId is provided, then HATEOAS links are embedded in each order.
    git-tfs-id: [http://ros-tfs-px01.picis.com:8080/tfs/emar]$/;C314

---
+++ WebAPI/API/C_Development/C_DevTrunk/Emar.Core/Sites/Repository/SiteRepository.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using Emar.Data;
+using Emar.Data.Entities;
+
+namespace Emar.Core.Sites.Repository
+{
+    public class SiteRepository : ISiteRepository
+    {
+        private EmarContext _context;
+
+        public SiteRepository(EmarContext context)
+        {
+            _context = context;
+        }
+
+        public Site GetSite(int siteId)
+        {
+            var site = _context.Sites.FirstOrDefault(s => s.Id == siteId);
+            if (site?.Name == null) return null;
+
+            return site;
+        }
+    }
+}
---
+++ WebAPI/API/C_Development/C_DevTrunk/Emar.Data/Entities/DepartmentPreferredListItem.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Emar.Data.Entities
+{
+    [Table("department_preferred_list_items")]
+    public partial class DepartmentPreferredListItem
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("site_id")]
+        public int SiteId { get; set; }
+
+        [Column("department_code")]
+        [StringLength(15)]
+        public string DepartmentCode { get; set; }
+
+        [Column("ndc")]
+        [StringLength(32)]
+        public string Ndc { get; set; }
+
+        [Required]
+        [Column("drug_id")]
+        [StringLength(32)]
+        public string DrugId { get; set; }
+
+        [Required]
+        [Column("brand_name")]
+        [StringLength(255)]
+        public string BrandName { get; set; }
+
+        [Column("dose", TypeName = "decimal(11, 2)")]
+        public decimal? Dose { get; set; }
+
+        [Column("medication_unit_id")]
+        public int? MedicationUnitId { get; set; }
+
+        [Column("medication_route_id")]
+        public int? MedicationRouteId { get; set; }
+
+        [Column("frequency_id")]
+        public int? FrequencyId { get; set; }
+
+        [Column("order_notes")]
+        public string OrderNotes { get; set; }
+
+
+        [ForeignKey(nameof(MedicationRouteId))]
+        [InverseProperty(nameof(Entities.MedicationRoute.DepartmentPreferredListItems))]
+        public virtual MedicationRoute MedicationRoute { get; set; }
+
+        [ForeignKey(nameof(MedicationUnitId))]
+        [InverseProperty(nameof(Entities.MedicationUnit.DepartmentPreferredListItems))]
+        public virtual MedicationUnit MedicationUnit { get; set; }
+
+        [ForeignKey(nameof(SiteId))]
+        [InverseProperty(nameof(Entities.Site.DepartmentPreferredListItems))]
+        public virtual Site Site { get; set; }
+    }
+}
