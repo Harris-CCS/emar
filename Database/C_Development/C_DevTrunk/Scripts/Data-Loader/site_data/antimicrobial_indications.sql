2342bba699478f441a7012120244738d6b645c9e
dm85223 <HARRIS\dm85223>
2020-08-06 01:22:48 +0000

    emar-168, emar-169
    git-tfs-id: [http://ros-tfs-px01.picis.com:8080/tfs/emar]$/;C323

---
+++ Database/C_Development/C_DevTrunk/Scripts/Data-Loader/site_data/antimicrobial_indications.sql
@@ -0,0 +1,104 @@
+print 'Loading Table: antimicrobial_indications';
+
+drop table if exists [#antimicrobial_indications];
+
+create table [#antimicrobial_indications]
+    (
+      [site_id]          [varchar](25) not null
+    , [code]             [varchar](20) not null
+    , [description]      [nvarchar](255) not null
+    , [is_active]        [bit] not null
+    , [ordinal_position] [int] not null);
+
+if '$(load_data)' = 'live'
+   and exists
+(
+    select null
+    from   [master].[sys].[databases]
+    where  [name] = 'ibex'
+)
+    begin
+
+        insert into [#antimicrobial_indications]
+            ([site_id]
+           , [code]
+           , [description]
+           , [is_active]
+           , [ordinal_position]
+            )
+        execute ('execute dbo.export_ibex_antimicrobial_indications');
+    end;
+
+if '$(load_data)' = 'sample'
+    begin
+
+        bulk insert [#antimicrobial_indications] from '$(current_path)Scripts\Data-Loader\sample_data\antimicrobial_indications.bcp' with(fieldterminator = '|~', rowterminator = '\n');
+    end;
+
+if
+(
+    select count(*)
+    from   [#antimicrobial_indications]
+) > 0
+    begin
+
+        begin transaction;
+
+/****************************************
+        load temporary tables for staging
+****************************************/
+
+        alter table [#antimicrobial_indications]
+        add [id]        [bigint] identity(1, 1)
+          , [target_id] [bigint];
+
+/********************************
+        get max id for seed value
+********************************/
+
+        set @max_id = null;
+
+        select @max_id = max([id])
+        from   [dbo].[antimicrobial_indications];
+
+        set @max_id = isnull(@max_id, 0);
+
+        update [source] set    
+            [target_id] = [source].[id] + @max_id
+        from   [#antimicrobial_indications] as [source];
+
+/*************************************
+        begin loading permanent tables
+*************************************/
+
+        -- set identity_insert [dbo].[antimicrobial_indications] on;
+
+        insert into [dbo].[antimicrobial_indications]
+            ([site_id]
+           , [code]
+           , [description]
+           , [is_active]
+           , [ordinal_position]
+            )
+        select isnull([internal_site].[id], -1) as [site_id]
+             , [source].[code]
+             , [source].[description]
+             , [source].[is_active]
+             , [source].[ordinal_position]
+        from   [#antimicrobial_indications] as [source]
+               outer apply [dbo].[get_internal_id]
+            ('pulsecheck', 'sites', [source].[site_id]) as [internal_site];
+
+        -- set identity_insert [dbo].[antimicrobial_indications] off;
+
+/***************************************
+        loading [external_ids] reference
+***************************************/
+/****************
+        end table
+****************/
+
+        commit transaction;
+    end;
+
+drop table if exists [#antimicrobial_indications];
---
+++ Database/C_Development/C_DevTrunk/Tables/antimicrobial_indication_items.sql
@@ -0,0 +1,120 @@
+create table [dbo].[antimicrobial_indication_items]
+    (
+      [id]           [int] identity(1, 1) not null
+    , [site_id]      [int] not null
+    , [sub_category] [int] not null
+    , constraint [pk__antimicrobial_indication_items__id] primary key clustered([id] asc));
+go
+
+/********
+ Defaults
+********/
+/*****************
+ Unique constraint
+*****************/
+/*******
+ Indexes
+*******/
+
+create nonclustered index [ix__antimicrobial_indication_items_site_id__Covered] on [dbo].[antimicrobial_indication_items]
+    ([site_id] asc) 
+      include
+    ([id], [sub_category]);
+go
+
+create unique nonclustered index [ix__antimicrobial_indication_items_SubcatSite_Covered] on [dbo].[antimicrobial_indication_items]
+    ([sub_category] asc, [site_id] asc) 
+      include
+    ([id]);
+go
+
+/***********
+ Foreign Key
+***********/
+
+alter table [dbo].[antimicrobial_indication_items]
+add constraint [fk__antimicrobial_indication_items__sites] foreign key([site_id]) references [dbo].[sites]([id]);
+go
+
+/***************
+ Data Dictionary
+    Defaults
+***************/
+/***************
+ Data Dictionary
+    Indexes
+***************/
+
+execute [sys].[sp_addextendedproperty] 
+    @name = N'MS_Description'
+  , @value = N'Index for MED_INDICATION_LIST table'
+  , @level0type = N'SCHEMA'
+  , @level0name = N'dbo'
+  , @level1type = N'TABLE'
+  , @level1name = N'antimicrobial_indication_items'
+  , @level2type = N'INDEX'
+  , @level2name = N'ix__antimicrobial_indication_items_site_id__covered';
+go
+
+execute [sys].[sp_addextendedproperty] 
+    @name = N'MS_Description'
+  , @value = N'Unique index for MED_INDICATION_LIST table'
+  , @level0type = N'SCHEMA'
+  , @level0name = N'dbo'
+  , @level1type = N'TABLE'
+  , @level1name = N'antimicrobial_indication_items'
+  , @level2type = N'INDEX'
+  , @level2name = N'ix__antimicrobial_indication_items_SubcatSite_Covered';
+go
+
+/***************
+ Data Dictionary
+    Table
+***************/
+
+execute [sys].[sp_addextendedproperty] 
+    @name = N'MS_Description'
+  , @value = N'id'
+  , @level0type = N'SCHEMA'
+  , @level0name = N'dbo'
+  , @level1type = N'TABLE'
+  , @level1name = N'antimicrobial_indication_items';
+go
+
+/***************
+ Data Dictionary
+    Columns
+***************/
+
+execute [sys].[sp_addextendedproperty] 
+    @name = N'MS_Description'
+  , @value = N'id'
+  , @level0type = N'SCHEMA'
+  , @level0name = N'dbo'
+  , @level1type = N'TABLE'
+  , @level1name = N'antimicrobial_indication_items'
+  , @level2type = N'COLUMN'
+  , @level2name = N'id';
+go
+
+execute [sys].[sp_addextendedproperty] 
+    @name = N'MS_Description'
+  , @value = N'Hospital identifier foriegn key to site table'
+  , @level0type = N'SCHEMA'
+  , @level0name = N'dbo'
+  , @level1type = N'TABLE'
+  , @level1name = N'antimicrobial_indication_items'
+  , @level2type = N'COLUMN'
+  , @level2name = N'site_id';
+go
+
+execute [sys].[sp_addextendedproperty] 
+    @name = N'MS_Description'
+  , @value = N'sub_cat'
+  , @level0type = N'SCHEMA'
+  , @level0name = N'dbo'
+  , @level1type = N'TABLE'
+  , @level1name = N'antimicrobial_indication_items'
+  , @level2type = N'COLUMN'
+  , @level2name = 'sub_category';
+go
