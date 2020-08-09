168b4fe171779a2c48fd88a3e34c29321196efb5
sg91136 <HARRIS\sg91136>
2020-08-09 21:30:56 +0000

    EMAR-187 default frequency value + some min time
    git-tfs-id: [http://ros-tfs-px01.picis.com:8080/tfs/emar]$/;C343

--- Presentation/C_Development/Work/Sylvie/emar/src/app/interfaces/order.ts
+++ Presentation/C_Development/Work/Sylvie/emar/src/app/interfaces/order.ts
@@ -1,11 +1,18 @@
+import { Frequency } from './frequency';
+import { Duration } from './duration';
+
+
 export interface Order {
     id: number,
     patientId: number,
-    startTime: string,
-    endTime: string,
     name: string,
     dose: string,
     route: string,
+    frequency?: Frequency,
+    duration?: Duration,
+    startTime: string,
+    endTime?: string,
+    triageTime?: string,
     signedOn: string,
     signedBy: string,
     allergies?: any,
