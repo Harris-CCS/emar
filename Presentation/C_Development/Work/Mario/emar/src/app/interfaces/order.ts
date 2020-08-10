165c5b804526827b4a9fdb76ce402b6bdc5cca01
mr88921 <mr88921@harris.tfs.local>
2020-08-10 13:22:57 +0000

    Merging DevTrunk - EMAR 187 Add in missing duration file
    git-tfs-id: [http://ros-tfs-px01.picis.com:8080/tfs/emar]$/;C350

--- Presentation/C_Development/Work/Mario/emar/src/app/interfaces/order.ts
+++ Presentation/C_Development/Work/Mario/emar/src/app/interfaces/order.ts
@@ -1,20 +1,19 @@
 import { Frequency } from './frequency';
-import { Duration } from './duration';
-
+// import { Duration } from './duration';
 
 export interface Order {
-    id: number,
-    patientId: number,
-    name: string,
-    dose: string,
-    route: string,
-    frequency?: Frequency,
-    duration?: Duration,
-    startTime: string,
-    endTime?: string,
-    triageTime?: string,
-    signedOn: string,
-    signedBy: string,
-    allergies?: any,
-    drugs?: any 
+  id: number;
+  patientId: number;
+  name: string;
+  dose: string;
+  route: string;
+  frequency?: Frequency;
+  duration?: Duration;
+  startTime: string;
+  endTime?: string;
+  triageTime?: string;
+  signedOn: string;
+  signedBy: string;
+  allergies?: any;
+  drugs?: any;
 }
