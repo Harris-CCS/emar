e6e6c66e0024e2e3eeabd482043d9c6c755451e1
mr88921 <mr88921@harris.tfs.local>
2020-08-18 13:21:06 +0000

    EMAR-180 Administration Instructions and formComponent reset (from Mario)
    git-tfs-id: [http://ros-tfs-px01.picis.com:8080/tfs/emar]$/;C399

--- Presentation/C_Development/C_DevTrunk/emar/src/app/interfaces/route.ts
+++ Presentation/C_Development/C_DevTrunk/emar/src/app/interfaces/route.ts
@@ -1,6 +1,6 @@
 export interface Route {
-    id: number;
-    siteId?: number;
-    routeName: string;
-    pointInTime?: Boolean;
+  id?: number;
+  siteId?: number;
+  routeName?: string;
+  pointInTime?: Boolean;
 }
