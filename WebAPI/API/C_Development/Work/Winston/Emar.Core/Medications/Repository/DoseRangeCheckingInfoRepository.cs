510af080a26bc9ab8d240249d4fd533b754c592c
Winston Murdock (HARRIS\wm85227) <WMurdock@harriscomputer.com>
2020-09-01 14:46:56 +0000

    EMAR-178: API for Dose Range Table Display
    git-tfs-id: [http://ros-tfs-px01.picis.com:8080/tfs/emar]$/;C444

---
+++ WebAPI/API/C_Development/Work/Winston/Emar.Core/Medications/Repository/DoseRangeCheckingInfoRepository.cs
@@ -0,0 +1,21 @@
+﻿using Emar.Data;
+using Emar.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Emar.Core.Medications.Repository
+{
+    public class DoseRangeCheckingInfoRepository : IDoseRangeCheckingInfoRepository
+    {
+        EmarContext _context;
+        public DoseRangeCheckingInfoRepository(EmarContext context)
+        {
+            _context = context;
+        } 
+        public IEnumerable<DoseRangeCheckingInfo> RetrieveDoseRangeCheckingInfo(string ndc)
+        {
+            return _context.DoseRangeCheckingInfos.FromSqlInterpolated($"exec pc_fdb_get_drc_info {ndc}").ToList();
+        }
+    }
+}
---
+++ WebAPI/API/C_Development/Work/Winston/Emar.Core/Medications/Repository/IDoseRangeCheckingInfoRepository.cs
@@ -0,0 +1,10 @@
+﻿using Emar.Data.Entities;
+using System.Collections.Generic;
+
+namespace Emar.Core.Medications.Repository
+{
+    public interface IDoseRangeCheckingInfoRepository
+    {
+        IEnumerable<DoseRangeCheckingInfo> RetrieveDoseRangeCheckingInfo(string ndc);
+    }
+}
